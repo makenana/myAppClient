ae5920719f0f24a6ca08e7e1be1c10619da4678b
makenana <makenabambrick17@gmail.com>
2024-03-12 14:15:35 -0700

    03/12/2024 added hello component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HelloComponent, pathMatch:'full'}
+];
