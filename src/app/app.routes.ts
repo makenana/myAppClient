9f2558bcca63e9bbce4a49f826a9e1ae9fb1f419
makenana <makenabambrick17@gmail.com>
2024-05-17 15:31:45 -0700

    last thing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
 import { CitiesComponent } from './cities/cities.component';
 import { CityParksComponent } from './cities/city-parks/city-parks.component';
+import { LoginComponent } from './auth/login.component';
 
 export const routes: Routes = [
     {path:'', component: HelloComponent, pathMatch:'full'},
     {path: 'cities', component: CitiesComponent},
-    {path: 'cityParks/:id', component: CityParksComponent}
+    {path: 'cityParks/:id', component: CityParksComponent},
+    {path: 'login', component: LoginComponent}
 ];
